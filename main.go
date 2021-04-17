ea64349bd8a1658e10f7ce595ff0539b6c94cead
Jeff DiNoto <jeff.dinoto@gmail.com>
2021-04-16 23:25:23 -0400

    * Add the auth package for GKE support * Add a script for an existing cluster (3-node, no ingress) * GoLand run-configuration

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	localauth "github.com/jdonenine/k8ssandra-api-service/pkg/auth"
 	"github.com/jdonenine/k8ssandra-api-service/pkg/controllers"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
